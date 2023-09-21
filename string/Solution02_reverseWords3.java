cbc3873dfe5f3795a7aa39b675233f06850ccb14
Akashkumar Yadav <ay355231@gmail.com>
2023-09-21 23:05:36 +0530

    Add leetecode Question reverseWords3.java

---
+++ string/Solution02_reverseWords3.java
@@ -0,0 +1,52 @@
+package com.string;
+
+public class Solution02_reverseWords3 {
+
+	 public static void reverse(char [] res , int left , int right){
+		 
+		    while(left<=right){
+		      char temp = res[left];
+		         res[left] = res[right];
+		        res[right]= temp;
+		        left++;
+		        right--;
+		    }
+		    
+
+  }
+	
+
+  public static String reverseWords(String str) 
+  {
+
+	    char [] res = str.toCharArray();
+
+	    int left = 0; 
+
+	    while(left < str.length()){
+
+	    int right = left ;
+
+	    while(right < str.length() && res[right] != ' '){
+
+	        right++;
+	   
+	    }
+
+	     reverse(res, left , right -1);
+	     left = right + 1 ;
+
+	    }    
+
+	    return String.valueOf(res);
+
+  }
+
+	 
+	public static void main(String [] args) {
+		
+		
+		
+	}
+	
+}
