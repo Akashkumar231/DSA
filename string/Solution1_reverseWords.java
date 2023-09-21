435dd670cefbea95ebe502637cbe88cf57722886
Akashkumar Yadav <ay355231@gmail.com>
2023-09-21 23:04:32 +0530

    Add reverseWords

--- string/Solution1_reverseWords.java
+++ string/Solution1_reverseWords.java
@@ -3,12 +3,56 @@ package com.string;
 //https://leetcode.com/problems/reverse-words-in-a-string/description/
 
 public class Solution1_reverseWords {
-
-	public static String reverseWords(String s) {
+    
+	public static String reverseWords2(String s) {
+		
+		int right = s.length()-1;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(right>=0) {
+			
+			while(right>= 0 && sb.charAt(right)==' ') {
+				
+				right --;
+				
+			}
+			
+	     	int	left = right ;
+			
+      		while(left>= 0 && sb.charAt(left)!=' ') {
+			
+			left--;
+			
+		     }
+			
+			if(sb.length()>0 && left<right) {
+				
+				sb.append(" ");
+				
+			}
+			
+			for(int index = left+1 ; index>=right ; index++) {
+				
+				sb.append(s.charAt(index));
+				
+			}
+			
+			right = left;
+			
+			
+		}
+		
+		return sb.toString();
+		
+	}
+	
+	
+	public static String reverseWords1(String s) {
 		
 		String [] words = s.trim().split("\s+");
 		
-		//trim: leading & trailing spaces : discard
+		//trim: leading & trailing  spaces : discard
 		//split on multiple spaces: single split.
 		
 		StringBuilder res = new StringBuilder();
