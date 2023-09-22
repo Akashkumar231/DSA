4faf6674273bd59232cd779f09f1d5f3868be163
Akashkumar Yadav <ay355231@gmail.com>
2023-09-22 12:30:43 +0530

    Make change to DemoPrograms.jaba

--- string/DemoPrograms.java
+++ string/DemoPrograms.java
@@ -2,4 +2,16 @@ package com.string;
 
 public class DemoPrograms {
 
+	public static void main(String [] args) {
+		
+		String demo = "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl";
+		
+		System.out.println(demo.length());
+		
+		System.out.println(demo.charAt(39*2));
+		
+		System.out.println(demo.charAt(39+1));
+
+	}
+	
 }
